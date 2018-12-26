c827400fec693432cc0bf9b57962d205cc0a19b3
Loegrath <kutor1124@yahoo.com>
2018-12-26 02:06:54 -0800

    Column update method finished. __repr__ needs finishing.

--- bin_clock_disp_wrap.py
+++ bin_clock_disp_wrap.py
@@ -1,5 +1,6 @@
 """Binary Clock Display Wrapper"""
 import numpy as np
+from datetime import datetime
 
 class clock_display():
     def __init__(self):
@@ -10,17 +11,48 @@ class clock_display():
         
         The clock is initialized to have all displays set to off.
         """
-        self.time = "Time not set."
+        self.time = datetime.today()
+        self.col_hours = np.zeros(4, dtype=int)
+        self.col_mins = np.zeros(4, dtype=int)
+        self.col_min_incs = np.zeros(4, dtype=int)
+        self.meridiem = False
+
+    def cols_reset(self):
+        """Reset all display columns to 0s."""
         self.col_hours = np.zeros(4, dtype=int)
         self.col_mins = np.zeros(4, dtype=int)
         self.col_min_incs = np.zeros(4, dtype=int)
 
-    def time_read(self):
-        # Code here
-
-    def time_set(self, time_str):
-        """Take an input and set the display to the correct time.
+    def cols_update(self):
+        """Update display columns so they correctly
+        represent the current time."""
+        translate_hours = [1, 2, 4, 8]
+        translate_mins = [1, 2, 4, 8]
+        translate_min_incs = [15, 30, 45, 60]
         
-        Function should take inputs as "HH:DD"
-        """
-        # Code here
+        self.cols_reset()
+
+        hour = self.time.hour
+        if hour == 0:
+            hour = 12
+        if hour > 12:
+            hour -= 12
+
+        for i in range(len(translate_hours) - 1, -1, -1):
+            if hour >= translate_hours[i]:
+                self.col_hours[i] = 1
+                hour -= translate_hours[i]
+
+        minutes = self.time.minute
+        for i in range(len(translate_min_incs) - 1, -1, -1):
+            if minutes >= translate_min_incs[i]:
+                self.col_min_incs[i] = 1
+                minutes -= translate_min_incs[i]
+        for i in range(len(translate_mins) - 1, -1, -1):
+            if minutes >= translate_mins[i]:
+                self.col_mins[i] = 1
+                minutes -= translate_mins[i]
+
+    def __repr__(self):
+        """Set repr to return the display of the binary clock."""
+        # Insert code here.
