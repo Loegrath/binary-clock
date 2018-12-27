15cc0a374b05268064877d0a349d77d842d72eb7
Loegrath <kutor1124@yahoo.com>
2018-12-26 16:57:37 -0800

    Created program for user.

---
+++ binary_clock.py
@@ -0,0 +1,13 @@
+"""Binary Clock"""
+from bin_clock_disp_wrap import clock_display, datetime
+
+clock = clock_display()
+
+time = datetime.now()
+clock.cols_update(time)
+print(clock)
+while True:
+    if time.minute is not datetime.now().minute:
+        time = datetime.now()
+        clock.cols_update(time)
+        print(clock)
